62706e13845ce272bcd7995791cdfe2b83dccc0f
Mikhail Mednikh <mmednikh@gmail.com>
2020-05-05 22:46:36 -0400

    tryide to create new employees tracker

--- Assets/employee-tracker.js
+++ Assets/employee-tracker.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   // Your username
   user: "root",
   // Your password
-  password: "Misha754",
+  password: "//",
   database: "employee_trackerDB"
 });
 connection.connect(async function(err) {
